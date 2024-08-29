21c805acaa5bdc6a0f38ed932af5db54e64777ba
Monarchuwu <75823146+Monarchuwu@users.noreply.github.com>
2024-08-30 00:49:13 +0700

    Add navigation and 3 fragments 3 fragments: LibraryFragment, SearchFragment, and GenerateAudioFragment

--- app/src/main/java/com/example/cs426_magicmusic/MainActivity.kt
+++ app/src/main/java/com/example/cs426_magicmusic/MainActivity.kt
@@ -5,16 +5,40 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main_layout)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        replaceFragment(LibraryFragment.newInstance())
+
+        val bottomNavigation: BottomNavigationView = findViewById(R.id.bottom_navigation)
+        bottomNavigation.setOnItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.navigation_library -> {
+                    replaceFragment(LibraryFragment.newInstance())
+                }
+                R.id.navigation_search -> {
+                    replaceFragment(SearchFragment.newInstance())
+                }
+                R.id.navigation_generate_audio -> {
+                    replaceFragment(GenerateAudioFragment.newInstance())
+                }
+            }
+            true
+        }
+    }
+
+    private fun replaceFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction().replace(R.id.main_fragment, fragment).commit()
     }
 }
